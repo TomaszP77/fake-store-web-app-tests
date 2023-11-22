a3b7a5f290f46827d7322f2ec0b9b76ade874cdc
Tomasz <feat24@wp.pl>
2023-11-22 14:44:03 +0100

    Add: addToCartTest

--- src/test/java/pages/WindsurfingCategoryPage.java
+++ src/test/java/pages/WindsurfingCategoryPage.java
@@ -1,4 +1,22 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+import static utils.DriverFactory.getDriver;
+
 public class WindsurfingCategoryPage {
+
+    @FindBy(css = "a[class=\"button product_type_simple add_to_cart_button ajax_add_to_cart\"]")
+    private WebElement addProductWindsurfingToCart;
+
+    public WindsurfingCategoryPage() {
+        PageFactory.initElements(new AjaxElementLocatorFactory(getDriver(), 10), this);
+    }
+
+    public void setAddProductWindsurfingToCart() {
+        addProductWindsurfingToCart.click();
+    }
 }
--- src/test/java/tests/ShoppingCartTests.java
+++ src/test/java/tests/ShoppingCartTests.java
@@ -1,4 +1,45 @@
 package tests;
 
-public class ShoppingCartTests {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pages.*;
+import utils.BaseTest;
+
+import static model.ReadyUsersWithData.userWithAccount;
+
+public class ShoppingCartTests extends BaseTest {
+
+    private HomePage homePage;
+    private MyAccountPage myAccountPage;
+    private LoggedUserPage loggedUserPage;
+    private ShopPage shopPage;
+    private WindsurfingCategoryPage windsurfingCategoryPage;
+    private CartPage cartPage;
+
+    @BeforeEach
+    public void beforeEach() {
+        setUp();
+        homePage = new HomePage();
+        myAccountPage = new MyAccountPage();
+        loggedUserPage = new LoggedUserPage();
+        shopPage = new ShopPage();
+        windsurfingCategoryPage = new WindsurfingCategoryPage();
+        cartPage = new CartPage();
+    }
+
+    @Test
+    public void addToCartTest() {
+        homePage.closingTheInformationWindow();
+        homePage.topMenuChoice("Moje konto");
+        myAccountPage.logInUser(userWithAccount());
+        homePage.topMenuChoice("Sklep");
+        shopPage.setShoppingCategoryChoice("Windsurfing");
+        windsurfingCategoryPage.setAddProductWindsurfingToCart();
+        cartPage.moveCursorTooCartQuickLook();
+        cartPage.viewCart();
+
+        Assertions.assertEquals("Egipt - El Gouna", cartPage.getTextForAssertionCart());
+        Assertions.assertTrue(cartPage.imageForAssertionCart());
+    }
 }
